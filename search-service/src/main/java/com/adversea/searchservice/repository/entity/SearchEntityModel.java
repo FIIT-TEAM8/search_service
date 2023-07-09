113df62a88cb930b8c23454e567c123704baaafa
Dominik Horvath <dom.horvath17@gmail.com>
2023-07-09 21:22:07 +0200

    dockerfile added

--- search-service/src/main/java/com/adversea/searchservice/repository/entity/SearchEntityModel.java
+++ search-service/src/main/java/com/adversea/searchservice/repository/entity/SearchEntityModel.java
@@ -3,6 +3,7 @@ package com.adversea.searchservice.repository.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -16,13 +17,19 @@ public class SearchEntityModel {
 
 
     @Field("name")
+    @Indexed
     private String name;
+
     @Field("name_ascii")
+    @Indexed
     private String nameAscii;
+
     @Field("type")
     private String type;
+
     @Field("locations")
     private Map<String, Integer> locations;
+
     @Field("information_source")
     private List<String> source;
 
